34dc33555cc62f7b9eb049d88e4b8bd2bc706823
yurychu <tesewar@mail.ru>
2015-11-23 22:14:31 +0300

    namespaces

--- two/hello.cpp
+++ two/hello.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+
 int main(){
-	using namespace std;
+	using std::cout;
+	using std::cin;
+	using std::endl;
+
 	cout << "Hello world!";
 	cout << endl;
 	cout << "Dont Worry! Be happy!" << endl;
