d0392e16e3b8eac9c0d4e17549986437e0a31971
yurychu <tesewar@mail.ru>
2015-11-26 22:28:12 +0300

    целочисленные типы

---
+++ three/limits.cpp
@@ -0,0 +1,26 @@
+// смотрим границы целых чисел
+#include <iostream>
+#include <climits>
+
+int main(){
+	using namespace std;
+
+	int n_int = INT_MAX;
+	short n_short = SHRT_MAX;
+	long n_long = LONG_MAX;
+
+	//используем sizeof
+	cout << "int is " << sizeof(int) << " bytes." << endl;
+	cout << "short is " << sizeof n_short << " bytes." << endl;
+	cout << "long is " << sizeof n_long << " bytes." << endl;
+	cout << "Maximum values: " << endl;
+	cout << "int: " << n_int << endl;
+	cout << "short: " << n_short << endl;
+	cout << "long: " << n_long << endl << endl;
+
+	cout << "Minimum int value: " << INT_MIN << endl;
+	cout << "Bits per byte: " << CHAR_BIT << endl;
+
+	return 0;
+}
+
