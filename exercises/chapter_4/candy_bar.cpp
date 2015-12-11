4ce1e78772fb5f4726c8f2459746386aea4452fe
yurychu <tesewar@mail.ru>
2015-12-11 20:01:14 +0300

    массив структур

--- exercises/chapter_4/candy_bar.cpp
+++ exercises/chapter_4/candy_bar.cpp
@@ -10,17 +10,27 @@ struct CandyBar {
 int main(){
 	using namespace std;
 
-	CandyBar snack = {
-		"Mocha Munch",
-		2.3,
-		350
+	CandyBar bars[3] = {
+		{"Mocha Munch", 2.3, 350},
+		{"Super Candy" , 3.0, 400},
+		{"Mega Sugar", 3.5, 500}
 	};
 
 	cout << "Тут у нас есть: \n"
-	<< "Название: " << snack.name << ". "
-	<< "Вес: " << snack.weight << ". "
-	<< "Калории: " << snack.calory << ". "
+	<< "Название: " << bars[0].name << ". "
+	<< "Вес: " << bars[0].weight << ". "
+	<< "Калории: " << bars[0].calory << ". "
 	<< endl;
+
+	cout << "Название: " << bars[1].name << ". "
+    << "Вес: " << bars[1].weight << ". "
+    << "Калории: " << bars[1].calory << ". "
+    << endl; 
+
+	cout << "Название: " << bars[2].name << ". "
+    << "Вес: " << bars[2].weight << ". "
+    << "Калории: " << bars[2].calory << ". "
+    << endl; 
 
 	return 0;
 }
