c48eeee37f030973af1675f9dce0e7406c986bd1
yurychu <tesewar@mail.ru>
2015-12-11 15:20:34 +0300

    начинаю делать упраженения главы 4

---
+++ exercises/chapter_4/self_control.cpp
@@ -0,0 +1,13 @@
+// вопросы для самоконтроля
+#include <iostream>
+
+int main(){
+	using namespace std;
+	char actor[30];
+	short betsie[100];
+	float chuck[13];
+	long double dipsea[64];
+
+	return 0;
+}
+
