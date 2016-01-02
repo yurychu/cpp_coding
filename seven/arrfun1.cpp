ea12a4880625498333bb8400155d5e5b80f83b43
yurychu <tesewar@mail.ru>
2016-01-02 17:34:33 +0300

    варианты исползования функции к массивам

--- seven/arrfun1.cpp
+++ seven/arrfun1.cpp
@@ -1,4 +1,4 @@
-// функция с аргументом массивом 
+// arrfun2 - функция с аргументом массивом 
 #include <iostream>
 
 const int ArSize = 8;
@@ -6,17 +6,25 @@ const int ArSize = 8;
 int sum_arr(int arr[], int n); // прототип
 
 int main(){
-	using namespace std;
-
 	int cookies[ArSize] = {1, 2, 4, 8, 16, 32, 64, 128};
 
+	std::cout << cookies << " = адрес массива, ";
+	std::cout << sizeof cookies << " = sizeof cookies\n";
 	int sum = sum_arr(cookies, ArSize);
-	cout << "Всего съедено печенья: " << sum << "\n";
+	std::cout << "Всего съедено печенья: " << sum << std::endl;
+	sum = sum_arr(cookies, 3);
+	std::cout << "Первые три едока съели: " << sum << " печенек.\n";
+	sum = sum_arr(cookies + 4, 4);
+	std::cout << "Последние 5 едоков съели: " << sum << " печенек.\n";
 	return 0;
 }
 
 int sum_arr(int arr[], int n){
 	int total = 0;
+
+	std::cout << arr << " = arr, ";
+	std::cout << sizeof arr << " = sizeof arr\n";
+
 	for (int i = 0; i < n; i++)
 		total = total + arr[i];
 	return total;
