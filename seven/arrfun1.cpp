b4a6882841e93f617351aeeea9357252c2467b64
yurychu <tesewar@mail.ru>
2016-01-02 20:52:56 +0300

    подход применения функций с указанием диапазона массива

--- seven/arrfun1.cpp
+++ seven/arrfun1.cpp
@@ -3,30 +3,27 @@
 
 const int ArSize = 8;
 
-int sum_arr(int arr[], int n); // прототип
+int sum_arr(const int *begin, const int *end); // прототип
 
 int main(){
 	int cookies[ArSize] = {1, 2, 4, 8, 16, 32, 64, 128};
 
-	std::cout << cookies << " = адрес массива, ";
-	std::cout << sizeof cookies << " = sizeof cookies\n";
-	int sum = sum_arr(cookies, ArSize);
+	int sum = sum_arr(cookies, cookies + ArSize);
 	std::cout << "Всего съедено печенья: " << sum << std::endl;
-	sum = sum_arr(cookies, 3);
+	sum = sum_arr(cookies, cookies + 3);
 	std::cout << "Первые три едока съели: " << sum << " печенек.\n";
-	sum = sum_arr(cookies + 4, 4);
+	sum = sum_arr(cookies + 4, cookies + 8);
 	std::cout << "Последние 5 едоков съели: " << sum << " печенек.\n";
 	return 0;
 }
 
-int sum_arr(int arr[], int n){
+int sum_arr(const int *begin, const int *end){
+	const int *pt;
+
 	int total = 0;
 
-	std::cout << arr << " = arr, ";
-	std::cout << sizeof arr << " = sizeof arr\n";
-
-	for (int i = 0; i < n; i++)
-		total = total + arr[i];
+	for (pt = begin; pt != end; pt++)
+		total = total + *pt;
 	return total;
 }
 
