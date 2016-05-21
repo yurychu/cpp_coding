529a43bcdf8c64edb3d5dd19c9ec913b16a4c5af
yurychu <tesewar@mail.ru>
2016-05-21 22:01:13 +0300

    freshing

---
+++ 21/01/provis_err.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(){
+    std::cout << "hljhl";
+    return 0;
+}
---
+++ 21/01/sum_mult.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+    using namespace std;
+
+    int x = 5;
+    int y = 7;
+    cout << endl;
+    cout << x + y << " " << x * y;
+    cout << endl;
+
+    return 0;
+}
