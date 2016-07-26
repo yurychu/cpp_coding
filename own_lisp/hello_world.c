b56d838fa8d194653a3ff960ca47c39b74b7acdb
yurychu <tesewar@mail.ru>
2016-07-27 01:33:54 +0300

    03_own

--- own_lisp/hello_world.c
+++ own_lisp/hello_world.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
+void print_hello(int n){
+    for (int i = 0; i < n; i++){
+        puts("hello!");
+    }
+}
+
 int main(int argc, char** argv){
-    puts("Hello world!");
+    print_hello(6);
     return 0;
 }
